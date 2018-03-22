6faa7205df873f508112b91e6820d29c9ddcfa63
Denis <carriere.denis@gmail.com>
2018-03-22 13:13:15 -0400

    Update sync-blocks.sh

--- cron/sync-blocks.sh
+++ cron/sync-blocks.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # EOS Scholar Testnet - Scheduled Scripts
+# Sync/Update EOS Blocks to AWS S3 bucket
+rm -f /tmp/blocks.tar.gz
 tar -zcvf /tmp/blocks.tar.gz /home/ubuntu/data-dir/blocks
-aws s3 cp /tmp/blocks.tar.gz s3://scholar.eosnation.io/blocks.tar.gz --acl public-read
+/usr/local/bin/aws s3 cp /tmp/blocks.tar.gz s3://scholar.eosnation.io/blocks.tar.gz --acl public-read
